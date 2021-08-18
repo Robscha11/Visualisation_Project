4c936dff2f04d15276a29de2e7e86d492e0c9fd9
PPrange <65134471+PPrange@users.noreply.github.com>
2021-08-18 22:06:45 +0200

    TXT Einlesen
    Habe testweise in die main was geschrieben um aus .txt  nen array zu machen. Weis aber noch nicht ob das für alle Funktioniert

--- Project-Code/public/main.js
+++ Project-Code/public/main.js
@@ -1,6 +1,30 @@
 import * as d3 from "d3";
 
+//lesen der TXT
+var Words = []; 
+var WordFile = new XMLHttpRequest(); 
+WordFile.open("GET", "data/CDU.txt", false); 
+WordFile.onreadystatechange = function () { if(WordFile.readyState === 4) { 
+  if(WordFile.status === 200 || WordFile.status == 0) { Words.push(WordFile.responseText.split(" ")); } } }; 
+WordFile.send(null);
 
-d3.csv("../data/AFD.csv").then((data) => {
-  console.log(data);
-});
+//paar sachen rausfiltern --- reihenfolge ist wichtig glaube ich
+var a = []
+Words.forEach(d => d.forEach(d => a.push(d.split("\n"))))
+a = a.flat()
+
+var b = []
+a.forEach(d => b.push(d.split("\t")))
+b = b.flat()
+
+var c = []
+b.forEach(d => c.push(d.split("\b")))
+c = c.flat()
+
+//Punkte filtern
+c = c.filter(function(n){ return n.includes(".") ? "" : n });
+//leere Elemente aus Array raus schmeißen
+c = c.filter(function(n){ return n != "" });
+
+
+console.log(c)
