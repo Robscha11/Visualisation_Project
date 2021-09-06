8506e5389b2234abdede1364dc8760cc5116fde3
Robin <trobby@web.de>
2021-09-06 20:58:03 +0200

    first steps, trouble with svg.attr

--- Project-Code/public/main.js
+++ Project-Code/public/main.js
@@ -3,7 +3,7 @@ import { getdocuments } from "./documents";
 import { getWordList } from "./wordList";
 import { gettfidf } from "./tfidf";
 import { wordcloud } from "./wordcloud";
-
+import { stackedBar } from "./stackedBar";
 
 var documents = getdocuments(); //alle Wörter
 var wordList = getWordList(documents); //jedes wort pro Partei nur EIN mal
@@ -26,5 +26,23 @@ wordcloud({
   selection: d3.select("#party2"),
   textColor: d3.rgb(0,255,0,255),
 });
+
+document.getElementById("button").onclick = function() {searchNode()};
+
+function searchNode() {
+  var selectedVal = document.getElementById("search").value
+  if (selectedVal == "none") {
+    console.log("nothing")
+  } else {
+    stackedBar({
+      svg: d3.select("#barchart"),
+      wordsPerGenre: tfidf,
+      search: selectedVal,
+      textColor: d3.rgb(0,255,0,255),
+    });
+  }
+}
+searchNode();
+
 
 
